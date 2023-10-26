9e22763b8b93ccbeaae0ad34eb1f91843e958c3f
Jonathon Broughton <jonathon@stardotbmp.com>
2023-10-26 18:08:22 +0100

    Traversal

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """This module contains the business logic of the function.
 
-use the automation_context module to wrap your function in an Autamate context helper
+use the automation_context module to wrap your function in an Automate context helper
 """
+from typing import Callable, cast
 
 from pydantic import Field
 from speckle_automate import (
@@ -9,8 +10,8 @@ from speckle_automate import (
     AutomationContext,
     execute_automate_function,
 )
-
-from flatten import flatten_base
+from specklepy.objects import Base
+from specklepy.objects.graph_traversal.traversal import TraversalRule, GraphTraversal
 
 
 class FunctionInputs(AutomateBase):
@@ -21,18 +22,17 @@ class FunctionInputs(AutomateBase):
     https://docs.pydantic.dev/latest/usage/models/
     """
 
-    forbidden_speckle_type: str = Field(
-        title="Forbidden speckle type",
+    forbidden_parameter_prefix: str = Field(
+        title="Parameter Prefix to Cleanse",
         description=(
-            "If a object has the following speckle_type,"
-            " it will be marked with an error."
+            "If a object has a parameter with the following prefix it will be removed from the object."
         ),
     )
 
 
 def automate_function(
-    automate_context: AutomationContext,
-    function_inputs: FunctionInputs,
+        automate_context: AutomationContext,
+        function_inputs: FunctionInputs,
 ) -> None:
     """This is an example Speckle Automate function.
 
@@ -40,50 +40,71 @@ def automate_function(
         automate_context: A context helper object, that carries relevant information
             about the runtime context of this function.
             It gives access to the Speckle project data, that triggered this run.
-            It also has conveniece methods attach result data to the Speckle model.
+            It also has convenience methods attach result data to the Speckle model.
         function_inputs: An instance object matching the defined schema.
     """
-    # the context provides a conveniet way, to receive the triggering version
+    # the context provides a convenient way, to receive the triggering version
+    if (
+            not function_inputs.forbidden_parameter_prefix
+            or function_inputs.forbidden_parameter_prefix == ""
+    ):
+        automate_context.mark_run_failed("No prefix has been set.")
+        return
+
     version_root_object = automate_context.receive_version()
 
-    count = 0
-    for b in flatten_base(version_root_object):
-        if b.speckle_type == function_inputs.forbidden_speckle_type:
-            if not b.id:
-                raise ValueError("Cannot operate on objects without their id's.")
+    func = get_default_traversal_func()
 
-            automate_context.attach_error_to_objects(
-                category="Forbidden speckle_type",
-                object_ids=b.id,
-                message="This project should not contain the type: "
-                f"{b.speckle_type}",
-            )
-            count += 1
+    traversal_collection_contexts = func.traverse(version_root_object)
 
-    if count > 0:
-        # this is how a run is marked with a failure cause
-        automate_context.mark_run_failed(
-            "Automation failed: "
-            f"Found {count} object that have one of the forbidden speckle types: "
-            f"{function_inputs.forbidden_speckle_type}"
-        )
+    for context in traversal_collection_contexts:
+        current = context.current
+        if hasattr(current, "parameters"):
+            parameters = cast(Base, current.parameters)
+            parameter_names = parameters.get_dynamic_member_names()
 
-    else:
-        automate_context.mark_run_success("No forbidden types found.")
+            for parameter_name in parameter_names:
+                if parameter_name.startswith(
+                        function_inputs.forbidden_parameter_prefix
+                ):
+                    # Base objects doesn't support the delitem method
+                    parameters.__dict__.pop(parameter_name)
 
-    # if the function generates file results, this is how it can be
-    # attached to the Speckle project / model
-    # automate_context.store_file_result("./report.pdf")
+    automate_context.create_new_version_in_project(
+        version_root_object,
+        "cleansed",
+        "This version has been cleansed of parameters with the prefix '"
+    )
 
 
-def automate_function_without_inputs(automate_context: AutomationContext) -> None:
-    """A function example without inputs.
+# traverse the root object and if a revit parameter has the prefix, remove it
+# this is a recursive function
 
-    If your function does not need any input variables,
-     besides what the automation context provides,
-     the inputs argument can be omitted.
+
+def get_default_traversal_func() -> GraphTraversal:
     """
-    pass
+    Traversal func for traversing a speckle commit object
+    """
+
+    DISPLAY_VALUE_PROPERTY_ALIASES = {"displayValue", "@displayValue"}
+    ELEMENTS_PROPERTY_ALIASES = {"elements", "@elements"}
+
+    display_value_rule = TraversalRule(
+        [
+            lambda o: any(
+                getattr(o, alias, None) for alias in DISPLAY_VALUE_PROPERTY_ALIASES
+            ),
+            lambda o: "Geometry" in o.speckle_type,
+        ],
+        lambda o: ELEMENTS_PROPERTY_ALIASES,
+    )
+
+    default_rule = TraversalRule(
+        [lambda _: True],
+        lambda o: o.get_member_names(),  # TODO: avoid deprecated members
+    )
+
+    return GraphTraversal([display_value_rule, default_rule])
 
 
 # make sure to call the function with the executor
